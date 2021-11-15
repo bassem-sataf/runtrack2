5ef7305166f9b383b02c7e152e8f8cbadcd3652d
sataf <bassem.sataf@laplateforme.io>
2021-11-15 22:58:11 +0100

    jour08 sauf job18

---
+++ jour08/job09.sql
@@ -0,0 +1,1 @@
+SELECT `id`, `prenom`, `nom`, `naissance`, `sexe`, `email` FROM `etudiants` WHERE naissance>'2003-11-10'
---
+++ jour08/job15.sql
@@ -0,0 +1,3 @@
+
+SELECT salles.nom , etage.nom FROM salles inner join etage ON salles.id_etage = etage.id 
+
